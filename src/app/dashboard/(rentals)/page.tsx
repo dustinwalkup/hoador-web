11f2f2b2288d930013a1b106b2fbbd08cd82ac15
Dustin Walkup <dustinwalkup@gmail.com>
2025-07-21 13:05:30 -0500

    remove rentals page

--- src/app/dashboard/(rentals)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function RentalsPage() {
-  // Redirect to the default rentals view (renting requests)
-  redirect("/dashboard/renting/requests");
-}
