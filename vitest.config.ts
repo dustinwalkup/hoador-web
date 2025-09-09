247106e7321e2206bb928913a22b84658e181541
Dustin Walkup <dustinwalkup@gmail.com>
2025-09-09 00:05:50 -0500

    implement husky

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import * as path from "path";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    globals: true,
+    setupFiles: [],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
